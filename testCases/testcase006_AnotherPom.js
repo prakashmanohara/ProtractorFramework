3ea504baf229e1031d189cc2c750cbf7a6601394
prakashmanohara <prakash.manohara@gmail.com>
2020-08-24 11:17:20 +0530

    added one more script for failure

--- testCases/testcase006_AnotherPom.js
+++ testCases/testcase006_AnotherPom.js
@@ -11,4 +11,11 @@ describe('Protractor_Demo_App', function () {
 
   })
 
+  it('addition_Test_With_Pob_Failure', function () {
+    homepage.openBrowser();
+    homepage.numberSet('1', '2');
+    homepage.clickGoButton();
+    homepage.verifyResult('1');
+  })
+
 });
